f558d739b72bc7c1035244a4f3b635b71db842ea
tomato13258 <160811525+tomato13258@users.noreply.github.com>
2024-03-13 13:20:51 +0800

    3/12 2

--- resources/js/Pages/TeacherManagement.vue
+++ resources/js/Pages/TeacherManagement.vue
@@ -9,20 +9,20 @@ import { Head } from '@inertiajs/vue3';
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">儀錶板</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">教師列表</h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex flex-col justify-center items-center">
-                    教師列表
+                    
                     <table class=" w-[80%]">
                         <tr class=" bg-red-400">
                             <th>序號</th>
                             <th>教師姓名</th>
                             <th>操作</th>
                         </tr>
-                        <tr class="text-center leading-[30px]">
+                        <tr class="text-center leading-[px]">
                             <td>1</td>
                             <td>Garry</td>
                             <td class="gap-5">
